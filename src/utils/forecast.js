886e377c99a1ecf2ce9c47ec1e81affb97f2964f
Mohdhadi-k <70443258+Mohdhadi-k@users.noreply.github.com>
2022-02-17 02:57:35 +0530

    added some additional info on forecast

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,7 +1,7 @@
 const request = require('request');
 
 const forecast = (latitude,longitude, callback) => {
-     const url = 'http://api.weatherstack.com/current?access_key=e386783ec5de0478cb3b75c68d595f57&query=' + latitude + ',' + longitude +'&units=s'
+     const url = 'http://api.weatherstack.com/current?access_key=e386783ec5de0478cb3b75c68d595f57&query=' + latitude + ',' + longitude +'&units=m'
      
      request ({url, json : true}, (error,{body}={}) =>{
             if (error){
@@ -15,8 +15,11 @@ const forecast = (latitude,longitude, callback) => {
                 const temperature =  body.current.temperature
                 const feelslike = body.current.feelslike
                 const description = body.current.weather_descriptions[0]
+                const windSpeed = body.current.wind_speed
+                const  windDir= body.current.wind_dir
+                const Vis = body.current.visibility
                 const data = {
-                    string : description + '. The temperature is ' + temperature + '. It feel like ' + feelslike
+                    string : description + '. The temperature is ' + temperature + 'ºC. It feel like ' + feelslike + 'ºC. The windspeed is ' + windSpeed + ' m/s in the ' + windDir + ' direction. The visibility is ' + Vis +' Km.'  
                 }
                 callback(undefined, data.string)
             }
